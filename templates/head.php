b673cd18f017e191e2056f6fbb0f2f2333c29e17
cPoledoUP <capoledo@up.edu.ph>
2022-06-07 20:59:44 +0800

    Added favicon

--- templates/head.php
+++ templates/head.php
@@ -15,3 +15,7 @@
 
     <link rel="stylesheet" href="css/base.css">
     <link rel="stylesheet" href="css/nav-footer.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="img/favicon/site.webmanifest">
