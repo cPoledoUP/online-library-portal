5adfd38d120354c87db540712b58c990ed2ed7c7
cPoledoUP <capoledo@up.edu.ph>
2022-06-07 22:27:55 +0800

    Reverted to old style browse books dropdown

--- templates/nav.php
+++ templates/nav.php
@@ -17,7 +17,15 @@ if (isset($_POST['nav-search-btn'])) {
         <ul>
             <li><a href="index.php"><button class="btn blue clickable"><i class="fa-solid fa-book-open"></i></button></a></li>
             <li><a href="help-support.php"><button class="btn blue clickable">Help & Support</button></a></li>
-            <li><a href="browse-books.php"><button class="btn blue clickable">Browse</button></a></li>
+            <li class="drop-menu blue">
+                <a href="browse-books.php">Browse</a><i class="fa-solid fa-angle-down"></i>
+                <ul>
+                    <li><a href="browse-books.php?sort-by=DateAdded">Browse by Date Added</a></li>
+                    <li><a href="browse-books.php?sort-by=Rating">Browse by Popularity</a></li>
+                    <li><a href="browse-books.php?sort-by=PubDate">Browse by Release Date</a></li>
+                    <li><a href="browse-books.php?sort-by=Title">Browse by Title</a></li>
+                </ul>
+            </li>
         </ul>
         <form class="search" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
             <input type="text" placeholder="Search..." name="search">
